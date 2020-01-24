830e9b83e7087845809370a2fc206a203c49ba94
digamber <digamberpradhan89@gmail.com>
2020-01-24 17:44:57 +0545

    Release for Subscription payment retry update

--- woocommerce-preview-emails.php
+++ woocommerce-preview-emails.php
@@ -5,7 +5,7 @@ Description: An Extension for WooCommerce that lets you Preview Emails, without 
 Plugin URI: https://www.digamberpradhan.com/preview-e-mails-for-woocommerce/
 Author: Digamber Pradhan
 Author URI: https://digamberpradhan.com/
-Version: 1.6.1
+Version: 1.6.2
 License: http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 WC requires at least: 3.0.0
 WC tested up to: 3.8.1
