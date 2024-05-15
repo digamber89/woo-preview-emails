10074a51147ebe69b6672e3c2870d73cd131587b
digamber pradhan <digamberpradhan89@gmail.com>
2024-05-15 15:57:43 +0545

    Add versioning

--- views/result/style.php
+++ views/result/style.php
@@ -1,6 +1,8 @@
 <?php
 /*Not the WordPress way but i'm trying to avoid any accidental CSS that would affect how the email template looks*/
+$assets         = include( WOO_PREVIEW_EMAILS_DIR . '/assets/main.asset.php' );
+$main_css       = add_query_arg( [ 'ver' => $assets['version'] ], $this->plugin_url . '/assets/main.css' );
+$style_main_css = add_query_arg( [ 'ver' => $assets['version'] ], $this->plugin_url . '/assets/style-main.css' );
 ?>
-
-<link rel="stylesheet" type="text/css" href="<?php echo $this->plugin_url . '/assets/main.css'; ?>">
-<link rel="stylesheet" type="text/css" href="<?php echo $this->plugin_url . '/assets/style-main.css'; ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo esc_url( $main_css ) ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo esc_url( $style_main_css ) ?>">
